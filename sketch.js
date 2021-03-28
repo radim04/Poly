8113f1a99d13c3d4778cf3c4f39560832009abed
radim04 <radim.sykora@sap.com>
2021-03-28 18:19:44 +0200

    gui

--- sketch.js
+++ sketch.js
@@ -1,8 +1,37 @@
+
+// gui params
+var myAngle = 30;
+var myColor = '#eeee00';
+
+var gui;
+
 function setup() {
-  createCanvas(400, 400);
+
+  createCanvas(windowWidth, windowHeight);
+
+  // Create the GUI
+  sliderRange(0, 90, 1);
+  gui = createGui('p5.gui');
+  gui.addGlobals('myColor', 'myAngle');
+
+  // Only call draw when then gui is changed
+  noLoop();
+
 }
 
+
 function draw() {
-  background(220);
-  ellipse(50,50,80,80);
+
+  // this is a piece of cake
+  background(0);
+  fill(myColor);
+  angleMode(DEGREES);
+  arc(width/2, height/2, 100, 100, myAngle/2, 360 - myAngle/2, PIE);
+
+}
+
+
+// dynamically adjust the canvas to the window
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
